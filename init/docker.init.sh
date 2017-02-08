4626e0fe96f2dfbf9ca5db91e2edc58a9eeb3225
kriscn <kris.cosmos@gmail.com>
2017-02-08 11:06:12 +0800

    Update docker.init.sh

--- init/docker.init.sh
+++ init/docker.init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-yum -y install vim wget ntpdate lrzsz
+yum -y install vim wget ntpdate lrzsz git
 ntpdate time.nist.gov
+echo "1 1 * * * /usr/sbin/ntpdate time.nist.gov" >> /var/spool/cron/root
+
 yum -y install docker
+curl -L https://github.com/docker/compose/releases/download/1.11.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 systemctl start docker.service
 systemctl status docker | grep active
